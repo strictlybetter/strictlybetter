ccbdb87e78d99d1ce48ff147ddea6df61d9305da
Henri Aho <dankirk_88@msn.com>
2019-05-29 19:37:00 +0300

    Change API formats to reduce un-necessary data. Use pagination for functional_reprints API.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -32,6 +32,28 @@ class ApiController extends Controller
 
 		$obsoletes = $obsoletes->orderBy('created_at', 'desc')->paginate(50);
 
+		$obsoletes->getCollection()->transform(function ($obsolete) {
+
+			return [
+				'id' => $obsolete->id,
+				'upvotes' => $obsolete->upvotes,
+				'downvotes' => $obsolete->downvotes,
+				'created_at' => $obsolete->created_at->toDateTimeString(),
+				'updated_at' => $obsolete->updated_at->toDateTimeString(),
+				'inferior' => [
+					'name' => $obsolete->inferior->name,
+					'multiverseid' => $obsolete->inferior->multiverse_id,
+					'functional_reprints_id' => $obsolete->superior->functional_reprints_id
+				],
+				'superior' => [
+					'name' => $obsolete->superior->name,
+					'multiverseid' => $obsolete->superior->multiverse_id,
+					'functional_reprints_id' => $obsolete->superior->functional_reprints_id
+				]
+
+			];
+		});
+
 	    return response()->json($obsoletes);
 	}
 
@@ -39,10 +61,13 @@ class ApiController extends Controller
 	{
 		$reprints = FunctionalReprint::with(['cards'])->paginate(200);
 
-		$formatted = [];
-		foreach ($reprints as $group) {
+		$reprints->getCollection()->transform(function ($group) {
 
-			$formatted[] = [
+			$cards = $group->cards->map(function ($card) {
+				return $card->only(['name', 'multiverse_id']);
+			});
+
+			return [
 				'id' => $group->id,
 				'typeline' => $group->typeline,
 				'manacost' => $group->manacost, 
@@ -50,11 +75,12 @@ class ApiController extends Controller
 				'toughness' => $group->toughness, 
 				'loyalty' => $group->loyalty, 
 				'rules' => $group->rules,
-				'cards' => $group->cards->pluck('name')
+				'cards' => $cards
 			];
-		}
 
-		return response()->json($formatted);
+		});
+
+		return response()->json($reprints);
 	}
 
 	public function guide()
