29bda8961ef951d5cb9c56ee69918e77e72a3e2f
Henri Aho <dankirk_88@msn.com>
2019-06-11 19:25:28 +0300

    Include navbar in main container to fix device orientation issues

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,44 +19,43 @@
         @yield('head')
     </head>
     <body>
+		<div class="container-fluid" style="padding:0">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			  <a class="navbar-brand" href="{{ route('index') }}">StrictlyBetter</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
 
-    	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		  <a class="navbar-brand" href="{{ route('index') }}">StrictlyBetter</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		    <ul class="navbar-nav mr-auto">
-		      <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
-		        <a class="nav-link" href="{{ route('index') }}">Browse</a>
-		      </li>
-		      <li class="nav-item {{ Request::is('deck') ? 'active' : '' }}">
-		        <a class="nav-link" href="{{ route('deck.index') }}">Upgrade Deck</a>
-		      </li>
-		      <li class="nav-item {{ Request::is('card', 'card/*') ? 'active' : '' }}">
-		        <a class="nav-link" href="{{ route('card.create') }}">Add Suggestion</a>
-		      </li>
-		      <li class="nav-item {{ Request::is('api-guide') ? 'active' : '' }}">
-		        <a class="nav-link" href="{{ route('api.guide') }}">API Guide</a>
-		      </li>
-		      <li class="nav-item {{ Request::is('about') ? 'active' : '' }}">
-		        <a class="nav-link" href="{{ route('about') }}">About</a>
-		      </li>
-		      <li class="nav-item {{ Request::is('changelog') ? 'active' : '' }}">
-		        <a class="nav-link" href="{{ route('changelog') }}">Changelog</a>
-		      </li>
-		    </ul>
-		    {{ Form::open(['id' => 'card-search-form', 'route' => 'card.search', 'autocomplete' => 'off', 'class' => 'form-inline my-2 my-lg-0']) }}
-		      <div class="ui-widget">
-		        <input id="card-search" name="term" class="form-control mr-sm-2" type="search" placeholder="Search Card" required minlength=2 aria-label="Search">
-		      </div>
-		      <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
-		    {{ Form::close() }}
-		  </div>
-		</nav>
-
-		<div class="container-fluid">
+			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			    <ul class="navbar-nav mr-auto">
+			      <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
+			        <a class="nav-link" href="{{ route('index') }}">Browse</a>
+			      </li>
+			      <li class="nav-item {{ Request::is('deck') ? 'active' : '' }}">
+			        <a class="nav-link" href="{{ route('deck.index') }}">Upgrade Deck</a>
+			      </li>
+			      <li class="nav-item {{ Request::is('card', 'card/*') ? 'active' : '' }}">
+			        <a class="nav-link" href="{{ route('card.create') }}">Add Suggestion</a>
+			      </li>
+			      <li class="nav-item {{ Request::is('api-guide') ? 'active' : '' }}">
+			        <a class="nav-link" href="{{ route('api.guide') }}">API Guide</a>
+			      </li>
+			      <li class="nav-item {{ Request::is('about') ? 'active' : '' }}">
+			        <a class="nav-link" href="{{ route('about') }}">About</a>
+			      </li>
+			      <li class="nav-item {{ Request::is('changelog') ? 'active' : '' }}">
+			        <a class="nav-link" href="{{ route('changelog') }}">Changelog</a>
+			      </li>
+			    </ul>
+			    {{ Form::open(['id' => 'card-search-form', 'route' => 'card.search', 'autocomplete' => 'off', 'class' => 'form-inline my-2 my-lg-0']) }}
+			      <div class="ui-widget">
+			        <input id="card-search" name="term" class="form-control mr-sm-2" type="search" placeholder="Search Card" required minlength=2 aria-label="Search">
+			      </div>
+			      <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+			    {{ Form::close() }}
+			  </div>
+			</nav>
+			
 			<br>
 			@if ($errors->any())
 				<div class="alert alert-danger alert-dismissible fade show" role="alert">
