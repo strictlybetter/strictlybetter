b4bcbfbf24fea68198621064344270bae0af3b1f
Henri Aho <dankirk_88@msn.com>
2019-09-07 05:15:46 +0300

    Remove unnecessary auth cookie and broadcast channel

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use /*AuthorizesRequests, */DispatchesJobs, ValidatesRequests;
 
     protected $request;
 
