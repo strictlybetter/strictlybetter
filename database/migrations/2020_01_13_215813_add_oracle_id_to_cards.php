3caf7e31ddb31ed62010b15063fd5ea6d9249fb6
Henri Aho <dankirk_88@msn.com>
2020-10-06 21:13:51 +0300

    Don't re-load Scryfall data during DB migration, if no cardss yet exist in the DB. Fixes new deployments needing the bulk-data file, which is not yet downloaded. #19

--- database/migrations/2020_01_13_215813_add_oracle_id_to_cards.php
+++ database/migrations/2020_01_13_215813_add_oracle_id_to_cards.php
@@ -13,37 +13,41 @@ class AddOracleIdToCards extends Migration
      */
     public function up()
     {
-		Schema::table('cards', function (Blueprint $table) {
-			$table->string('oracle_id')->nullable();
-		});
+        Schema::table('cards', function (Blueprint $table) {
+            $table->string('oracle_id')->nullable();
+        });
 
-		// Load cards again to populate oracle_id
-		Artisan::call('load-scryfall', ['callbacks' => ['query' => function($obj, $parent = null) {
+        // Update existing cards if any in database
+        if (App\Card::count() > 0) {
 
-			// Find existing card with same name, or scryfall_api attribute (unless this is a card face with a parent card)
-			$q = App\Card::query();
+            // Load cards again to populate oracle_id
+            Artisan::call('load-scryfall', ['callbacks' => ['query' => function($obj, $parent = null) {
 
-			if ($parent) {
-				$q = $q->where('main_card_id', $parent->id)->where('name', $obj->name);
-			}
-			else {
-				$q = $q->whereNull('main_card_id')->where(function($q) use ($obj) {
-					$q->where('name', $obj->name)->orWhere('scryfall_api', $obj->uri);
-				});
-			}
-			return $q->orderBy('created_at', 'desc')->first();
+                // Find existing card with same name, or scryfall_api attribute (unless this is a card face with a parent card)
+                $q = App\Card::query();
 
-        }]]);
+                if ($parent) {
+                    $q = $q->where('main_card_id', $parent->id)->where('name', $obj->name);
+                }
+                else {
+                    $q = $q->whereNull('main_card_id')->where(function($q) use ($obj) {
+                        $q->where('name', $obj->name)->orWhere('scryfall_api', $obj->uri);
+                    });
+                }
+                return $q->orderBy('created_at', 'desc')->first();
 
-		// Remove bad (duplicate) cards we may have created earlier using the old naming method
-		Artisan::call('remove-old-spoilers', []);
+            }]]);
 
-		// Replace index
-		Schema::table('cards', function (Blueprint $table) {
-			$table->dropIndex('cards_scryfall_api_index');
-			$table->index('oracle_id');
-		});
-	}
+            // Remove bad (duplicate) cards we may have created earlier using the old naming method
+            Artisan::call('remove-old-spoilers', []);
+        }
+
+        // Replace index
+        Schema::table('cards', function (Blueprint $table) {
+            $table->dropIndex('cards_scryfall_api_index');
+            $table->index('oracle_id');
+        });
+    }
 
     /**
      * Reverse the migrations.
@@ -53,7 +57,7 @@ class AddOracleIdToCards extends Migration
     public function down()
     {
         Schema::table('cards', function (Blueprint $table) {
-        	$table->dropColumn('oracle_id');
+            $table->dropColumn('oracle_id');
             $table->index('scryfall_api');
         });
     }
