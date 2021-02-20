56091fb27d57b72e749b013e74b18b0c16365117
Henri Aho <dankirk_88@msn.com>
2021-02-20 18:32:21 +0200

    Fix typeline migration not working with future updates (shouldn't use load-scryfall in migration)

--- database/migrations/2021_02_09_120343_add_typeline_to_cards.php
+++ database/migrations/2021_02_09_120343_add_typeline_to_cards.php
@@ -20,7 +20,25 @@ class AddTypelineToCards extends Migration
         if (App\Card::count() > 0) {
 
             // Load typeline
-            Artisan::call('load-scryfall', []);
+            {
+                $cards = App\Card::select(['id', 'main_card_id', 'supertypes', 'types', 'subtypes', 'typeline'])->get();
+                foreach ($cards as $card) {
+                    $typeline = "";
+                    if (!empty($card->supertypes)) {
+                        $typeline = $typeline . implode(" ", $card->supertypes) . (empty($card->types) ? "" : " ");
+                    }
+
+                    if (!empty($card->types))
+                        $typeline = $typeline . implode(" ", $card->types);
+
+                    if (!empty($card->subtypes))
+                        $typeline = $typeline . ' — ' . implode(" ", $card->subtypes);
+
+                    $card->timestamps = false;
+                    $card->typeline = $typeline;
+                    $card->save();
+                }
+            }
 
             // Reprints use wrong hyphen, upadte it
             $reprints = App\FunctionalReprint::with(['cards'])->get();
