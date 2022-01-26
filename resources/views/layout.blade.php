4e802e6b50d09f9f1a3fb0f243f0cdd1b9d06405
Henri Kulotie <dankirk_88@msn.com>
2022-01-27 00:40:32 +0200

    Re-add hamburger menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -34,6 +34,9 @@
 	<body>
 		<div class="container-fluid" style="padding:0">
 			<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 				<a class="navbar-brand" href="{{ route('index') }}">Strictly Better</a>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav mr-auto">
